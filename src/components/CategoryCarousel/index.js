248cd72a4a4deb9603cf1969329e8ecb35885a0e
Higor Rodrigues <higor_1992@hotmail.com>
2023-01-06 20:06:26 -0300

    Criando Carrossel de categorias e estilizando

--- src/components/CategoryCarousel/index.js
+++ src/components/CategoryCarousel/index.js
@@ -1,24 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import Category from "../../assets/CATEGORIAS.png";
 
-import { Container, CategoryImg } from "./styles";
+import {
+  Container,
+  CategoryImg,
+  ContainerItems,
+  Image,
+  Button,
+} from "./styles";
+
 import api from "../../services/api";
+import Carousel from "react-elastic-carousel";
 
 function CategoryCarousel() {
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
     async function loadCategories() {
-      const response = await api.get("categories");
+      const { data } = await api.get("categories");
 
-      console.log(response);
+      setCategories(data);
     }
 
     loadCategories();
   }, []);
 
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 400, itemsToShow: 2 },
+    { width: 600, itemsToShow: 3 },
+    { width: 900, itemsToShow: 4 },
+    { width: 1300, itemsToShow: 5 },
+  ];
+
   return (
     <Container>
       <CategoryImg src={Category} alt="Logo da categoria" />
+
+      <Carousel
+        itemsToShow={5}
+        style={{ width: "90%" }}
+        breakPoints={breakPoints}
+      >
+        {categories &&
+          categories.map((category) => (
+            <ContainerItems key={category.id}>
+              <Image src={category.url} alt="foto da categoria" />
+              <Button>{category.name}</Button>
+            </ContainerItems>
+          ))}
+      </Carousel>
     </Container>
   );
 }
--- src/components/CategoryCarousel/styles.js
+++ src/components/CategoryCarousel/styles.js
@@ -1,5 +1,67 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: #efefef;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 35px;
+  padding: 35px 0;
+
+  .rec.rec-arrow {
+    background-color: #9758a6;
+    color: #efefef;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    border: none;
+  }
+
+  .rec.rec-arrow:hover {
+    border: 2px solid #9758a6;
+    background-color: #efefef;
+    color: #9758a6;
+  }
+
+  .rec.rec-arrow:disabled {
+    border: none;
+    background-color: #bebebf;
+    color: #efefef;
+  }
+`;
 
 export const CategoryImg = styled.img``;
+
+export const ContainerItems = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Image = styled.img`
+  width: 200px;
+  border-radius: 10px;
+`;
+
+export const Button = styled.button`
+  margin-top: 16px;
+  background: #9758a6;
+  border-radius: 8px;
+  height: 50px;
+  border: none;
+  cursor: pointer;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 100%;
+
+  text-align: center;
+
+  color: #ffffff;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:active {
+    opacity: 0.6;
+  }
+`;
