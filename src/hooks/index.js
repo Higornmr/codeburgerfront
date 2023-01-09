04f967f036481fa77eea9edc309df03c01aa8b81
Higor Rodrigues <higor_1992@hotmail.com>
2023-01-09 13:00:18 -0300

    criado a lógica do carrinho de compras

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,8 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { UserProvider } from "./UserContext";
+import { CartProvider } from "./CartContext";
 
-const AppProvider = ({ children }) => <UserProvider>{children}</UserProvider>;
+const AppProvider = ({ children }) => (
+  <UserProvider>
+    <CartProvider>{children}</CartProvider>
+  </UserProvider>
+);
 
 AppProvider.propTypes = {
   children: PropTypes.node,
