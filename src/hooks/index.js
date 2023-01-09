d3cdf66fb8b22ab95d29cfe358162c86ae0edbb6
Higor Rodrigues <higor_1992@hotmail.com>
2023-01-09 11:31:04 -0300

    criando arquivo index.js na pasta hooks para  alter as importações dos hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { UserProvider } from "./UserContext";
+
+const AppProvider = ({ children }) => <UserProvider>{children}</UserProvider>;
+
+AppProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export default AppProvider;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { ToastContainer } from "react-toastify";
 
 import Routes from "./routes/routes";
 import GlobalStyles from "./styles/globalStyles";
-import { UserProvider } from "./hooks/UserContext";
+import AppProvider from "./hooks";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <UserProvider>
+    <AppProvider>
       <Routes />
-    </UserProvider>
+    </AppProvider>
     <ToastContainer autoClose={2000} theme="dark" />
     <GlobalStyles />
   </>
