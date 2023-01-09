6ceb13db79d11786520dd7cce21d5a888d6ccd7a
Higor Rodrigues <higor_1992@hotmail.com>
2023-01-09 14:16:11 -0300

    estilizando o carrinho

--- src/components/Cartitems/index.js
+++ src/components/Cartitems/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useCart } from "../../hooks/CartContext";
-import { Container, Header, Body } from "./styles";
+import { Container, Header, Body, EmptyCart } from "./styles";
 import formatCurrency from "../../utils/formatCurrency";
 
 export function CartItems() {
@@ -12,11 +12,11 @@ export function CartItems() {
         <p></p>
         <p>Itens</p>
         <p>Preço</p>
-        <p>Quantidade</p>
+        <p style={{ paddingRight: 30 }}>Quantidade</p>
         <p>Total</p>
       </Header>
 
-      {cartProducts &&
+      {cartProducts && cartProducts.length > 0 ? (
         cartProducts.map((product) => (
           <Body key={product.id}>
             <img src={product.url} />
@@ -25,7 +25,10 @@ export function CartItems() {
             <p>{product.quantity}</p>
             <p>{formatCurrency(product.quantity * product.price)}</p>
           </Body>
-        ))}
+        ))
+      ) : (
+        <EmptyCart>Carrinho vazio</EmptyCart>
+      )}
     </Container>
   );
 }
