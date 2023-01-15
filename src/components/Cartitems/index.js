0c95ab668e7e9db279e04707bd5e5bff47dadcac
Higor Rodrigues <higor_1992@hotmail.com>
2023-01-15 09:07:48 -0300

    dando funcionalidade aos botões de adicionar ou remover itens do carrinho de produtos

--- src/components/Cartitems/index.js
+++ src/components/Cartitems/index.js
@@ -4,7 +4,7 @@ import { Container, Header, Body, EmptyCart } from "./styles";
 import formatCurrency from "../../utils/formatCurrency";
 
 export function CartItems() {
-  const { cartProducts } = useCart();
+  const { cartProducts, increaseProducts, decreaseProducts } = useCart();
   console.log(cartProducts);
   return (
     <Container>
@@ -23,9 +23,9 @@ export function CartItems() {
             <p>{product.name}</p>
             <p>{formatCurrency(product.price)}</p>
             <div className="quantity-container">
-              <button>-</button>
+              <button onClick={() => decreaseProducts(product.id)}>-</button>
               <p>{product.quantity}</p>
-              <button>+</button>
+              <button onClick={() => increaseProducts(product.id)}>+</button>
             </div>
 
             <p>{formatCurrency(product.quantity * product.price)}</p>
--- src/hooks/CartContext.js
+++ src/hooks/CartContext.js
@@ -30,6 +30,45 @@ export const CartProvider = ({ children }) => {
     );
   };
 
+  const deleteProducts = async (productId) => {
+    const newCart = cartProducts.filter((product) => product.id !== productId);
+
+    setCartProducts(newCart);
+
+    await localStorage.setItem("codeburger:cartInfo", JSON.stringify(newCart));
+  };
+
+  const increaseProducts = async (productId) => {
+    const newCart = cartProducts.map((product) => {
+      return product.id === productId
+        ? { ...product, quantity: product.quantity + 1 }
+        : product;
+    });
+
+    setCartProducts(newCart);
+
+    await localStorage.setItem("codeburger:cartInfo", JSON.stringify(newCart));
+  };
+
+  const decreaseProducts = async (productId) => {
+    const cartIndex = cartProducts.findIndex((pd) => pd.id === productId);
+    if (cartProducts[cartIndex].quantity > 1) {
+      const newCart = cartProducts.map((product) => {
+        return product.id === productId
+          ? { ...product, quantity: product.quantity - 1 }
+          : product;
+      });
+      setCartProducts(newCart);
+
+      await localStorage.setItem(
+        "codeburger:cartInfo",
+        JSON.stringify(newCart)
+      );
+    } else {
+      deleteProducts(productId);
+    }
+  };
+
   useEffect(() => {
     const loadUserData = async () => {
       const clientCartData = await localStorage.getItem("codeburger:cartInfo");
@@ -43,7 +82,14 @@ export const CartProvider = ({ children }) => {
   }, []);
 
   return (
-    <CartContext.Provider value={{ putProductInCart, cartProducts }}>
+    <CartContext.Provider
+      value={{
+        putProductInCart,
+        cartProducts,
+        increaseProducts,
+        decreaseProducts,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
