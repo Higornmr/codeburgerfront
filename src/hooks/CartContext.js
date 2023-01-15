94e5ec5049d67498fc15ea4bb2dfd474ded71d5d
Higor Rodrigues <higor_1992@hotmail.com>
2023-01-15 09:15:03 -0300

    Atualizando local storage, em uma unica função e chamando ela nos lugares onde ficava o local storage

--- src/hooks/CartContext.js
+++ src/hooks/CartContext.js
@@ -7,6 +7,10 @@ const CartContext = createContext({});
 export const CartProvider = ({ children }) => {
   const [cartProducts, setCartProducts] = useState([]);
 
+  const updateLocalStorage = async (products) => {
+    await localStorage.setItem("codeburger:cartInfo", JSON.stringify(products));
+  };
+
   const putProductInCart = async (product) => {
     const cartIndex = cartProducts.findIndex((prd) => prd.id === product.id);
 
@@ -24,10 +28,7 @@ export const CartProvider = ({ children }) => {
       setCartProducts(newCartProducts);
     }
 
-    await localStorage.setItem(
-      "codeburger:cartInfo",
-      JSON.stringify(newCartProducts)
-    );
+    await updateLocalStorage(newCartProducts);
   };
 
   const deleteProducts = async (productId) => {
@@ -35,7 +36,7 @@ export const CartProvider = ({ children }) => {
 
     setCartProducts(newCart);
 
-    await localStorage.setItem("codeburger:cartInfo", JSON.stringify(newCart));
+    await updateLocalStorage(newCart);
   };
 
   const increaseProducts = async (productId) => {
@@ -47,7 +48,7 @@ export const CartProvider = ({ children }) => {
 
     setCartProducts(newCart);
 
-    await localStorage.setItem("codeburger:cartInfo", JSON.stringify(newCart));
+    await updateLocalStorage(newCart);
   };
 
   const decreaseProducts = async (productId) => {
@@ -60,10 +61,7 @@ export const CartProvider = ({ children }) => {
       });
       setCartProducts(newCart);
 
-      await localStorage.setItem(
-        "codeburger:cartInfo",
-        JSON.stringify(newCart)
-      );
+      await updateLocalStorage(newCart);
     } else {
       deleteProducts(productId);
     }
