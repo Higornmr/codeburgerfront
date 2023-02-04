b14a90a7cf27adfdcd7748762cd40f5b7c2cd962
Higor Rodrigues <higor_1992@hotmail.com>
2023-02-04 11:15:33 -0300

    adicionado, estilizado e dado a função n botão de logout na tela de pedidos do ADM, e iniciando a tela de listagem de produtos

--- src/components/SideMenuAdmin/index.js
+++ src/components/SideMenuAdmin/index.js
@@ -1,19 +1,28 @@
 import React from "react";
 import listLink from "./menu-list";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 import { Container, ItemContainer, ListLink } from "./styles";
+import { useUser } from "../../hooks/UserContext";
 
 export function SideMenuAdmin() {
+  const { logout } = useUser();
   return (
     <Container>
       <hr></hr>
       {listLink.map((item) => (
-        <ItemContainer key={item.id}>
+        <ItemContainer key={item.id} isActive={true}>
           <item.icon className="icon" />
           <ListLink to={item.link}>{item.label}</ListLink>
         </ItemContainer>
       ))}
       <hr></hr>
+      <ItemContainer style={{ position: "absolute", bottom: "30px" }}>
+        <LogoutIcon style={{ color: "#fff" }} />
+        <ListLink to="/login" onClick={logout}>
+          Sair
+        </ListLink>
+      </ItemContainer>
     </Container>
   );
 }
