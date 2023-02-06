54363e190c6212baacc81d57eef9f80a3ceb0e20
Higor Rodrigues <higor_1992@hotmail.com>
2023-02-06 15:58:17 -0300

    Criando formulário de adição de produto

--- src/containers/Admin/NewProducts/index.js
+++ src/containers/Admin/NewProducts/index.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
+import ReactSelect from "react-select";
+import { Button } from "../../../components";
+import { useForm } from "react-hook-form";
 
 import api from "../../../services/api";
 
-import { Container } from "./styles";
+import { Container, Label, Input } from "./styles";
 
 function NewProduct() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   useEffect(() => {
     async function loadOrders() {
       const { data } = await api.get("products");
@@ -15,7 +21,20 @@ function NewProduct() {
 
   return (
     <Container>
-      <div>olá</div>
+      <form noValidate>
+        <Label>Nome</Label>
+        <Input type="text" {...register("name")} />
+
+        <Label>Preço</Label>
+        <Input type="number" {...register("price")} />
+
+        <Label>Upload</Label>
+        <Input type="file" accept="image/png, image/jpeg" />
+
+        <ReactSelect />
+
+        <Button>Adicionar Produto</Button>
+      </form>
     </Container>
   );
 }
