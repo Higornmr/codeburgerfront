0325b6cc3281e83e44460e50f3823929e3ff444a
Higor Rodrigues <higor_1992@hotmail.com>
2023-02-07 16:43:15 -0300

    Finalizando tela de edição de usuários

--- src/containers/Admin/EditProduct/index.js
+++ src/containers/Admin/EditProduct/index.js
@@ -10,7 +10,14 @@ import * as Yup from "yup";
 
 import { ErrorMessage } from "../../../components";
 import api from "../../../services/api";
-import { Container, Label, Input, ButtonStyled, LabelUpload } from "./styles";
+import {
+  Container,
+  Label,
+  Input,
+  ButtonStyled,
+  LabelUpload,
+  ContainerInput,
+} from "./styles";
 
 function EditProduct() {
   const [fileName, setFileName] = useState(null);
@@ -22,24 +29,11 @@ function EditProduct() {
     },
   } = useHistory();
 
-  console.log(product);
-
   const schema = Yup.object().shape({
     name: Yup.string().required("Digite o nome do produto"),
     price: Yup.string().required("Digite o preçõ do produto"),
     category: Yup.object().required("Escolha uma categoria"),
-    file: Yup.mixed()
-      .test("required", "Carregue um arquivo", (value) => {
-        return value?.length > 0;
-      })
-      .test("filesize", "Carregue arquivos de até 2mb", (value) => {
-        return value[0]?.size <= 2000000;
-      })
-      .test("type", "Carregue apenas arquivos JPEG, ou PNG", (value) => {
-        return (
-          value[0]?.type === "image/jpeg" || value[0]?.type === "image.png"
-        );
-      }),
+    offer: Yup.bool(),
   });
 
   const {
@@ -57,12 +51,16 @@ function EditProduct() {
     productDataFormData.append("price", data.price);
     productDataFormData.append("category_id", data.category.id);
     productDataFormData.append("file", data.file[0]);
+    productDataFormData.append("offer", data.offer);
 
-    await toast.promise(api.post("products", productDataFormData), {
-      pending: "Criando novo produto...",
-      success: "Produto criado com sucesso",
-      error: "Falhar ao criar o produto",
-    });
+    await toast.promise(
+      api.put(`products/${product.id}`, productDataFormData),
+      {
+        pending: "Editando novo produto...",
+        success: "Produto editado com sucesso",
+        error: "Falhar ao editar o produto",
+      }
+    );
 
     setTimeout(() => {
       push("/listar-produtos");
@@ -72,7 +70,6 @@ function EditProduct() {
   useEffect(() => {
     async function loadCategories() {
       const { data } = await api.get("categories");
-
       setCategories(data);
     }
     loadCategories();
@@ -83,12 +80,20 @@ function EditProduct() {
       <form noValidate onSubmit={handleSubmit(onSubmit)}>
         <div>
           <Label>Nome</Label>
-          <Input type="text" {...register("name")} />
+          <Input
+            type="text"
+            {...register("name")}
+            defaultValue={product.name}
+          />
           <ErrorMessage>{errors.name?.message}</ErrorMessage>
         </div>
         <div>
           <Label>Preço</Label>
-          <Input type="number" {...register("price")} />
+          <Input
+            type="number"
+            {...register("price")}
+            defaultValue={product.price}
+          />
           <ErrorMessage>{errors.price?.message}</ErrorMessage>
         </div>
         <div>
@@ -115,6 +120,7 @@ function EditProduct() {
           <Controller
             name="category"
             control={control}
+            defaultValue={product.category}
             render={({ field }) => {
               return (
                 <ReactSelect
@@ -123,13 +129,23 @@ function EditProduct() {
                   getOptionLabel={(cat) => cat.name}
                   getOptionValue={(cat) => cat.id}
                   placeholder="Categoria"
+                  defaultValue={product.category}
                 />
               );
             }}
           ></Controller>
           <ErrorMessage>{errors.category?.message}</ErrorMessage>
         </div>
-        <ButtonStyled>Adicionar Produto</ButtonStyled>
+
+        <ContainerInput>
+          <input
+            type="checkbox"
+            {...register("offer")}
+            defaultChecked={product.offer}
+          />
+          <Label>Produto em oferta ?</Label>
+        </ContainerInput>
+        <ButtonStyled>Editar Produto</ButtonStyled>
       </form>
     </Container>
   );
